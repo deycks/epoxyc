3cb97e20ba8c190a572bfed086e143b2f38e53dd
Edgar Adrian Muñoz Morales <edgar.munoz@neoris.com>
2024-04-23 15:56:32 -0600

    fix inital banner

--- src/components-landing/Hero.js
+++ src/components-landing/Hero.js
@@ -18,7 +18,7 @@ export default function Hero() {
           theme.palette.mode === 'light'
             ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
             : `linear-gradient(#02294F, ${alpha('#090E10', 0.0)})`,
-        backgroundSize: '100% 20%',
+        backgroundSize: '100% 60%',
         backgroundRepeat: 'no-repeat',
       })}
     >
