15a20b707bd920a8abb10175b0b4e8697dcd17fc
Edgar Adrian Muñoz Morales <edgar.munoz@neoris.com>
2024-05-29 12:11:38 -0600

    modify sender email

--- src/utils/nodemailer.js
+++ src/utils/nodemailer.js
@@ -4,6 +4,7 @@ export const transporter = nodemailer.createTransport({
     host: "smtp.hostinger.com",
     port: 465,
     secure: true, // use TLS
+    requireTLS: true,
     auth: {
         user: "contact@upscalegarage.com",
         pass: "Contacto.123",
@@ -12,7 +13,8 @@ export const transporter = nodemailer.createTransport({
 
 export const mailOptions={
     from:"contact@upscalegarage.com",
-    to:"ed.adrianmm@gmail.com"
+    bcc:"ed.adrianmm@gmail.com",
+    to:"jrayo19@icloud.com"
 }
 
 //process.env.EMAIL_USER
